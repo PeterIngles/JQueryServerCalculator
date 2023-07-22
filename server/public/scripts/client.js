d7cd7ccd3b33f2192484a3c210be988989692dd4
Peter Ingles <peter.ingles@gmail.com>
2023-07-22 14:51:11 -0500

    Added Btn handlers

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,43 +1,53 @@
 $(document).ready(onReady)
 
-let serverPackage = {};
+// Declare out serverPackage as an object
+
+
+let solution = {};
 
 function onReady() {
-    $('.add-button').on('click', addFood)
+    $('#enterBtn').on('click', sendNumbers)
+    $('#plusBtn').on('click', plusSelector)
+    $('#minusBtn').on('click', minusSelector)
+    $('#multiplyBtn').on('click', multiplySelector)
+    $('#divideBtn').on('click', divideSelector)
 
-    console.log('He Jquery is working')
-    getFood()
+    console.log('Hey Jquery is working')
+    getSolution()
 }
 
-let addFood = (event) => {
+
+
+
+// Create our POST function that will send the serverPackage to the server
+let sendNumbers = (event) => {
     event.preventDefault();
-    console.log('inside addFood')
-    const newFood = {
-        food: $("#itemInput").val(),
-        description: $("#descriptionInput").val()
+    console.log('inside sendNumbers')
+    const sentNumbers = {
+        num1: $("#num1Input").val(),
+        num2: $("num2Input").val(),
+        operation: $()
     }
 
     // AJAX to communicate and send data to server
 
     $.ajax({
         method: "POST", // type of request
-        url: "/addfood", //route that will be used
-        data: newFood // Needs to be an object
+        url: "/sendnumbers", //route that will be used
+        data: serverPackage // Needs to be an object
 
     }).then((response) => {
         console.log("POST was successful", response) //Expect 201
-        getFood()
+        getSolution()
         render()
     }).catch((error) => {
         console.log("Error with POST request", error)
         alert("Error with POST")
     })
-    $('#itemInput').val("")
-    $('#descriptionInput').val("")
     // Take some data and send to server
 }
 
-let getFood = () => {
+let getSolution = () => {
 
     // Use Ajax to retrieve (GET) quotes from server
         // Server endpooint: /quotes
